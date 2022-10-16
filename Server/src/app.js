7b1435ee58de440f3257e6bf65b171d6cd0afa98
JeHeeYu <sees1234558@gmail.com>
2022-10-16 15:58:05 +0900

    Create Node Project

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,1 +1,14 @@
+const http = require('http');
 const express = require('express');
+//const db = require('../models');
+
+app = express();
+
+const port = 3000;
+
+app.set('port', port);
+
+
+app.listen(app.get('port'), () => {
+    console.log(`Start Server on Port : ${port}`);
+});
