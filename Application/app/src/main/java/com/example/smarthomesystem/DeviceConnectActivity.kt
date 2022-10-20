dbc57bf93786efedb676a8afe6e81e0c93f84384
JeHeeYu <sees1234558@gmail.com>
2022-10-20 21:44:51 +0900

    Add back press click event

--- Application/app/src/main/java/com/example/smarthomesystem/DeviceConnectActivity.kt
+++ Application/app/src/main/java/com/example/smarthomesystem/DeviceConnectActivity.kt
@@ -12,6 +12,7 @@ import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import com.example.smarthomesystem.databinding.ActivityDeviceConnectBinding
@@ -20,6 +21,7 @@ import com.example.smarthomesystem.databinding.ActivityDeviceConnectBinding.infl
 class DeviceConnectActivity : AppCompatActivity() {
 
     lateinit var deviceConnectBinding : ActivityDeviceConnectBinding
+    var lastTimeBackPressed : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         deviceConnectBinding = ActivityDeviceConnectBinding.inflate(layoutInflater)
@@ -28,14 +30,30 @@ class DeviceConnectActivity : AppCompatActivity() {
 
         val connection = NetworkConnection(application)
 
+        deviceConnectBinding.connectButton.setOnClickListener {
+            moveWifiSettingScreen()
+        }
+
         connection.observe(this, Observer { isConnected ->
             if(isConnected) {
                 // WiFI에 연결되어 있을 때
+                Log.e("jehee", "jehee2")
             }
             else {
                 // WiFI에 연결되어 있지 않을 때
             }
         })
+    }
+
+    // 뒤로 가기 버튼 클릭
+    override fun onBackPressed() {
+        if(System.currentTimeMillis() - lastTimeBackPressed >= 1500) {
+            lastTimeBackPressed = System.currentTimeMillis()
+            Toast.makeText(this,"'뒤로' 버튼을 한번 더 누르시면 종료됩니다.",Toast.LENGTH_LONG).show()
+        }
+        else {
+            finish()
+        }
     }
 
     fun moveWifiSettingScreen()
